c7fe340ac1bf6c4f8e6fba7a39ceb3e711536a46
Oleksii Trekhleb <trehleb@gmail.com>
2020-03-19 06:27:46 +0100

    Implement the dashboard with COVID-19 trends per country.

---
+++ js/data.js
@@ -0,0 +1,18 @@
+const dataURL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series';
+const dataURLs = {
+  confirmed: `${dataURL}/time_series_19-covid-Confirmed.csv`,
+  deaths: `${dataURL}/time_series_19-covid-Deaths.csv`,
+  recovered: `${dataURL}/time_series_19-covid-Recovered.csv`,
+};
+
+function loadData() {
+  return Promise
+    .all(Object.values(dataURLs).map((dataURL) => fetch(dataURL)))
+    .then((responses) => Promise.all(
+      responses.map(response => response.text()))
+    )
+    .then((data) => data.reduce((acc, val, index) => {
+      acc[Object.keys(dataURLs)[index]] = val;
+      return acc;
+    }, {}));
+}
---
+++ js/index.js
@@ -0,0 +1,34 @@
+// Globals.
+const r = React;
+const rd = ReactDOM;
+const e = r.createElement;
+
+document.addEventListener('DOMContentLoaded', () => {
+  rd.render(e(App), document.getElementById('root'));
+});
+
+function App() {
+  const [isLoading, setIsLoading] = r.useState(false);
+  const [data, setData] = r.useState(null);
+  r.useEffect(() => {
+    setIsLoading(true);
+    loadData().then((data) => {
+      setData(data);
+      setIsLoading(false);
+    });
+  }, []);
+  if (isLoading) {
+    return e(Spinner);
+  }
+  return e('div', null, 'data');
+}
+
+function Button({onClick = () => {}, children = null}) {
+  const type = 'button';
+  const className = 'btn btn-dark';
+  return e('button', {type, className, onClick}, children);
+}
+
+function Spinner() {
+  return e('div', {className: 'spinner-border'});
+}
