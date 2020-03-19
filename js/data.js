cee49eb44e6247d2096251aed8e39e2cbb894c04
Oleksii Trekhleb <trehleb@gmail.com>
2020-03-19 07:56:38 +0100

    Implement the dashboard with COVID-19 trends per country.

--- js/data.js
+++ js/data.js
@@ -1,18 +1,46 @@
-const dataURL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series';
-const dataURLs = {
-  confirmed: `${dataURL}/time_series_19-covid-Confirmed.csv`,
-  deaths: `${dataURL}/time_series_19-covid-Deaths.csv`,
-  recovered: `${dataURL}/time_series_19-covid-Recovered.csv`,
+const covidDataTypes = {
+  confirmed: {
+    key: 'confirmed',
+    title: 'Confirmed',
+    backgroundColor: 'rgba(255, 159, 64, 0.2)',
+    borderColor: 'rgba(255, 159, 64, 1)',
+  },
+  recovered: {
+    key: 'recovered',
+    title: 'Recovered',
+    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+    borderColor: 'rgba(75, 192, 192, 1)',
+  },
+  deaths: {
+    key: 'deaths',
+    title: 'Deaths',
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgba(255, 99, 132, 1)',
+  },
 };
 
-function loadData() {
+const covidCountries = {
+  all: {
+    key: 'all',
+    title: 'All',
+  }
+};
+
+const covidDataBaseURL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series';
+const covidDataURLs = {
+  [covidDataTypes.confirmed.key]: `${covidDataBaseURL}/time_series_19-covid-Confirmed.csv`,
+  [covidDataTypes.recovered.key]: `${covidDataBaseURL}/time_series_19-covid-Recovered.csv`,
+  [covidDataTypes.deaths.key]: `${covidDataBaseURL}/time_series_19-covid-Deaths.csv`,
+};
+
+function loadCovidData() {
   return Promise
-    .all(Object.values(dataURLs).map((dataURL) => fetch(dataURL)))
+    .all(Object.values(covidDataURLs).map((dataURL) => fetch(dataURL)))
     .then((responses) => Promise.all(
       responses.map(response => response.text()))
     )
     .then((data) => data.reduce((acc, val, index) => {
-      acc[Object.keys(dataURLs)[index]] = Papa.parse(val).data;
+      acc[Object.keys(covidDataURLs)[index]] = Papa.parse(val).data;
       return acc;
     }, {}));
 }
--- js/index.js
+++ js/index.js
@@ -8,19 +8,48 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function App() {
-  const [isLoading, setIsLoading] = r.useState(false);
-  const [data, setData] = r.useState(null);
+  const [covidData, setCovidData] = r.useState(null);
   r.useEffect(() => {
-    setIsLoading(true);
-    loadData().then((data) => {
-      setData(data);
-      setIsLoading(false);
-    });
+    loadCovidData().then((data) => setCovidData(data));
   }, []);
-  if (isLoading) {
+  if (!covidData) {
     return e(Spinner);
   }
-  return e('div', null, 'data');
+  return e('div', null, e(CovidChart, {covidData}));
+}
+
+function CovidChart({
+  covidData,
+  countries = [covidCountries.all.key],
+  types = Object.keys(covidDataTypes),
+}) {
+  const canvasRef = r.useRef(null);
+  r.useEffect(() => {
+    if (!canvasRef.current) {
+      return;
+    }
+    const ctx = canvasRef.current.getContext('2d');
+    const datasets = types.map((type) => {
+      return {
+        label: covidDataTypes[type].title,
+        data: covidData[type][5].slice(4).map(count => parseInt(count, 10)),
+        borderWidth: 1,
+        borderColor: covidDataTypes[type].borderColor,
+        backgroundColor: covidDataTypes[type].backgroundColor,
+        fill: false,
+      };
+    });
+    const labels = covidData[covidDataTypes.confirmed.key][0].slice(4);
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels,
+        datasets,
+      },
+      options: {},
+    });
+  }, [types, countries]);
+  return e('canvas', {height: 100, ref: canvasRef});
 }
 
 function Spinner() {
